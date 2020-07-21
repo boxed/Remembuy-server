d40cefcc856fdc81f756cc4106b45f5e1da27665
Anders Hovmöller <boxed@killingar.net>
2020-07-21 13:50:08 +0200

    .

--- remembuy/settings.py
+++ remembuy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$gsegg4ni@n$ehr*g_s5udtkui!-%2z6771%5#5jljdt_+=8p^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'remembuy.kodare.com',
