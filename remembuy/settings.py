327d659338d9ce04add95ee5fa0c0f4c2a0ae02e
Anders Hovmöller <boxed@killingar.net>
2020-07-21 08:25:49 +0200

    .

--- remembuy/settings.py
+++ remembuy/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from pathlib import Path
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -115,6 +117,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = Path(__file__).parent.parent / 'static/'
 
 
 # Static files (CSS, JavaScript, Images)
